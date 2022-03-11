3e6203a4da07661382db42782d37586abb66ed7b
이율희 <07yumeno@gmail.com>
2022-03-12 03:13:05 +0900

    문자열 equals

--- pro03/src/exam02/Sample09.java
+++ pro03/src/exam02/Sample09.java
@@ -1,5 +1,7 @@
 package exam02;
 
+import java.util.Scanner;
+
 public class Sample09 {
 
 	public static void main(String[] args) {
@@ -19,6 +21,25 @@ public class Sample09 {
 		s = "Hello Java Programming";
 		int len = s.length();
 		System.out.println("문자열의 문자수 : " + len);
+		
+		/*
+		 * 문자열과 문자열 비교하는 방법
+		 * 		문자열변수명.equals(문자열)
+		 * 
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.nextLine();
+		String str2 = "문자열";
+		
+		if(str2.equals(str1)) {
+			System.out.println("equals동일한 문자열 값 입니다.");
+		}
+		
+//		문자열의 특징때문에 ==을 쓸 수 없다. 반드시 equals를 써야한다.
+//		if(str2 == str1) {
+//			System.out.println("==로 비교한 문자열 값 입니다. 비교 결과 거짓으로 나온다.");
+//		}
 
 	}
 

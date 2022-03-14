4df36c3bf94c89f2fab619ececa45b0657928ca7
이율희 <07yumeno@gmail.com>
2022-03-14 19:07:58 +0900

    문자열 배열 수정

---
+++ pro04/src/exam01/Sample03.java
@@ -0,0 +1,43 @@
+package exam01;
+
+public class Sample03 {
+
+	public static void main(String[] args) {
+		
+		// 문자열 배열 생성1
+		String[] arr1 = new String[3];
+		arr1[0] = "문자열"; arr1[1] = "배열"; arr1[2] = "사용";
+		
+		//문자열 배열 생성2
+		String[] arr2 = {"문자열", "배열", "생성"};
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.print("\n");
+		
+		
+		//문자열 배열 생성, 초기화 하지 않음
+		String[] arr3 = new String[3];
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print(arr3[i] + "\t");
+			
+			//문자열 배열의 값을 초기화 하지 않으면 문자열 관련 메서드를 사용할 때 에러 발생.
+			//java.lang.NullPointerException
+			System.out.println(arr3[i].length());
+		}
+		System.out.print("\n");
+		
+		//정수 배열 생성, 초기화 하지 않음
+		int[] arr4 = new int[3];
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.print(arr4[i] + "\t");
+		}
+		System.out.print("\n");
+		
+
+	}
+
+}

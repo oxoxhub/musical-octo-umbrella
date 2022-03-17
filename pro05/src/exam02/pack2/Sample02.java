cf7b13d272f857bac68b3aef057b2ad203749b97
이율희 <07yumeno@gmail.com>
2022-03-17 17:59:58 +0900

    객체 주석 추가

--- pro05/src/exam02/pack2/Sample02.java
+++ pro05/src/exam02/pack2/Sample02.java
@@ -1,13 +1,14 @@
 package exam02.pack2;
 
-import exam02.pack1.PublicAccessTest;
+//import exam.02.pack1.*; 는 pack1의 모든 클래스를 import한다.
+import exam02.pack1.PublicAccessTest; //import문이 없다면 아래처럼 패키지명 생략을 못하고 적어줘야한다.
 import exam02.pack1.DefaultAccessTest;
 //The type exam02.pack1.DefaultAccessTest is not visible
 
 public class Sample02 {
 
 	public static void main(String[] args) {
-		PublicAccessTest p = new PublicAccessTest();
+		exam02.pack1.PublicAccessTest p = new exam02.pack1.PublicAccessTest();
 		DefaultAccessTest d = new DefaultAccessTest();
 	}
 

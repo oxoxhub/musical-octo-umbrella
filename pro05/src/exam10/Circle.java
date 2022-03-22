2ffc6aae1ab28dadf05f6fe68797c67170fe3e99
이율희 <07yumeno@gmail.com>
2022-03-22 19:44:29 +0900

    다이어그램 실습 예제 3 수정

--- pro05/src/exam10/Circle.java
+++ pro05/src/exam10/Circle.java
@@ -2,32 +2,29 @@ package exam10;
 
 public class Circle {
 	
-	private double PI = 3.14;
+	private final double PI = 3.14;
 	private int radius = 1;
-	private double area;
-	private double size;
+//	private double area;
+//	private double size;
 	
 	public Circle() {
 		//기본생성자
-		getAreaOfCircle();
-		getSizeOfCircle();
 	}
 	
 	public void incrementRadius() {
-		this.radius++;
-		getAreaOfCircle();
-		getSizeOfCircle();
+		//반지름의 크기를 1 증가시키는 메서드
+		radius++;
 	}
 	
 	public void getAreaOfCircle() {
 		//원의 넓이를 구하는 메서드
-		area = PI * radius * radius;
+		double area = PI * radius * radius;
 		System.out.println("원의 넓이 : " + area);
 	}
 	
 	public void getSizeOfCircle() {
 		//원의 둘레를 구하는 메서드
-		size = 2 * PI * radius;
+		double size = 2 * PI * radius;
 		System.out.println("원의 둘레 : " + size);
 	}
 	
--- pro05/src/exam10/Run3.java
+++ pro05/src/exam10/Run3.java
@@ -5,7 +5,11 @@ public class Run3 {
 	public static void main(String[] args) {
 		
 		Circle c = new Circle();
+		c.getSizeOfCircle();
+		c.getAreaOfCircle();
 		c.incrementRadius();
+		c.getSizeOfCircle();
+		c.getAreaOfCircle();
 	}
 
 }

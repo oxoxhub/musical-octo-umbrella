6fdaeeda5802c390b3a2f957ce9928a2daa15e93
이율희 <07yumeno@gmail.com>
2022-04-16 03:08:39 +0900

    업앤다운 게임

--- pro12/src/game/Fail.java
+++ pro12/src/game/Fail.java
@@ -1,10 +1,17 @@
 package game;
 
+import java.util.Random;
+
 public class Fail extends Result {
+
+	private String[] penalty = new String[] {
+			"소주 1잔 마시기", "소주 2잔 마시기",
+			"옆자리에 가서 소주 1잔 받아 먹기"
+	};
+	private Random rand = new Random(); 
 	
 	@Override
 	public String toString() {
-		return "실패";
+		return "실패 " + penalty[rand.nextInt(penalty.length)];
 	}
-
 }

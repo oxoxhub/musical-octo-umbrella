577ad6fc402de13e668fab7bfcf6366fd65bfba6
이율희 <07yumeno@gmail.com>
2022-04-22 01:45:27 +0900

    StringTokenizer 활용하기

--- pro08/src/exam01/Sample04.java
+++ pro08/src/exam01/Sample04.java
@@ -12,11 +12,14 @@ public class Sample04 {
 		String phone = "010-1234-5678";
 		StringTokenizer token = new StringTokenizer(phone, "-");
 		
+		//countTokens : nextToken()으로 분리할 수 있는 토큰의 수를 반환
+		//				StringTokenizer객체 안에 담겨있는 토큰들의 개수를 반환해주는 메서드
 		System.out.println("토큰 수 : " + token.countTokens());
 		
 		//배열에 담는 것을 생략한다.
+		//hasMoreTokens : 분리할 수 있는 토큰의 존재 유무를 boolean형으로 반환
 		while(token.hasMoreTokens()) {
-			System.out.println(token.nextToken());
+			System.out.println(token.nextToken());	//nextToken : 분리된 토큰을 반환
 		}
 	}
 

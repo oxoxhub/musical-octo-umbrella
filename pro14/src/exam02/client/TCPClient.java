c3d8f6bab6afb59fd15edb85194fc1be12426e2d
이율희 <07yumeno@gmail.com>
2022-06-06 23:14:35 +0900

    TCP Client

--- pro14/src/exam02/client/TCPClient.java
+++ pro14/src/exam02/client/TCPClient.java
@@ -18,9 +18,11 @@ public class TCPClient {
 			/*
 			 * 1. 네트워크 통신을 위한 IP주소 정보 설정
 			 */
+			//getByAddress : byte배열을 통해 IP주소를 얻는다.
 			byte[] addr = new byte[] {(byte)58,(byte)127,(byte)192,(byte)220};
 			InetAddress serverIp = InetAddress.getByAddress(addr);
 			int serverPort = 50000;
+//			String serverIp = InetAddress.getLocalHost().getHostAddress();
 			
 			/*
 			 * 2. 클라이언트용 소켓 생성
@@ -35,19 +37,33 @@ public class TCPClient {
 			
 			Scanner sc = new Scanner(System.in);
 			while(true) {
-				System.out.println("사용자 입력: ");
+				System.out.println("사용자 입력 >>> ");
 				String str = sc.nextLine();
-				sOut.write(str);
-				sOut.newLine();
+				
+				if(str.equals("exit")) {
+					sOut.write(str + "\n");
+					sOut.flush();
+					break;
+				}
+				
+				//정상 메세지
+				sOut.write(str + "\n");
+//				sOut.newLine();
 				sOut.flush();
+				
+				//읽기
+				String msg = sIn.readLine();
+				System.out.println("서버 : " + msg);
+				
 				
 			}
 			/*
 			 * 4. 데이터 송신을 완료하였으면 모든 자원 반납
 			 */
-//			sIn.close();
-//			sOut.close();
-//			sock.close();
+			sc.close();	
+			sIn.close();
+			sOut.close();
+			sock.close();
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();

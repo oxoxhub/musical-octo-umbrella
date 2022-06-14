254f6d51530d9c264e1ed67199e8aedfa74c9322
이율희 <07yumeno@gmail.com>
2022-06-14 22:48:45 +0900

    자바스크립트 - 이벤트

---
+++ web_pro09/src/main/webapp/static/js/sample06.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+function f1() {
+	var id1 = document.getElementById("id1");
+	id1.innerText = "클릭 이벤트 동작!!!";
+}
+
+function f2(element) {
+	element.innerText = "클릭 이벤트 동작!!!";
+}
+//element는 관습이다. 매개변수명은 아무거나 상관없다.

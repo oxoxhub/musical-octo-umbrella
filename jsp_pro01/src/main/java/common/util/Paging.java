be79e7d7c73a7fcf36b128ae81ea6f6b075e0b6c
이율희 <07yumeno@gmail.com>
2022-08-01 22:26:17 +0900

    JSP / Servlet board paging

---
+++ jsp_pro01/src/main/java/common/util/Paging.java
@@ -0,0 +1,69 @@
+package common.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paging {
+
+	private int offset;				// 시작 위치
+	private int limit;				// 목록수 제한
+	private int currentPageNumber;	// 현재 페이지
+	private int nextPageNumber;		// 다음 페이지
+	private int prevPageNumber;		// 이전 페이지
+	private List<Integer> pageNumberList;	//페이지 리스트
+	private List<Object> pageData;	//현재 페이지의 데이터
+	
+	public Paging(List<Object> datas, int currentPageNumber, int limit, int totalRow) {
+		this.offset = limit * (currentPageNumber - 1) + 1 ;
+		this.currentPageNumber = currentPageNumber;
+		this.limit = limit;
+		this.nextPageNumber = currentPageNumber + 1;
+		this.prevPageNumber = currentPageNumber - 1;
+		int pageNum = 1;
+		this.pageNumberList = new ArrayList<Integer>();
+		for(int i = 0; i < totalRow; i += limit) {
+			this.pageNumberList.add(pageNum++);
+		}
+		this.pageData = datas.subList(this.offset, this.offset + this.limit - 1);
+	}
+	
+	public int getOffset() {
+		return offset;
+	}
+	
+	public int getLimit() {
+		return limit;
+	}
+	
+	public int getCurrentPageNumber() {
+		return currentPageNumber;
+	}
+	
+	public int getNextPageNumber() {
+		return nextPageNumber;
+	}
+	
+	public int getPrevPageNumber() {
+		return prevPageNumber;
+	}
+	
+	public List<Integer> getPageNumberList() {
+		return pageNumberList;
+	}
+	
+	public List<Integer> getPageNumberList(int start, int end) {
+		return pageNumberList.subList(start - 1, end);
+	}
+	
+	public List<Object> getPageData() {
+		return pageData;
+	}
+	
+	public boolean hasNextPage() {
+		return this.pageNumberList.contains(this.nextPageNumber);
+	}
+	
+	public boolean hasPrevPage() {
+		return this.pageNumberList.contains(this.prevPageNumber);
+	}
+}

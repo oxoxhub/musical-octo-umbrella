e2154bc186ef0d1ea37d7e6c237f9b7113ed80a5
이율희 <07yumeno@gmail.com>
2022-08-20 00:46:34 +0900

    spring logging aop

--- sp_pro01/src/main/java/com/myhome/web/aop/LoggingAOP.java
+++ sp_pro01/src/main/java/com/myhome/web/aop/LoggingAOP.java
@@ -1,6 +1,10 @@
 package com.myhome.web.aop;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -8,19 +12,50 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Component	//bean 등록을 위한 어노테이션
-@Aspect		//아래있는 클래스는 aop 클래스다.
+@Component
+@Aspect
 public class LoggingAOP {
-	
-	private static final Logger logger = LoggerFactory.getLogger(LoggingAOP.class);
 
-	// 조인포인트 설정 -> 포인트컷으로 설정 : 포인트컷은 조인포인트의 집합.
-	@Pointcut(value="execution(* com.myhome.web.*.controller.*Controller.*(..))")
-	private void loggingCut() {}
+	private static final Logger logger = LoggerFactory.getLogger(LoggingAOP.class);
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS");
+	
+	// 조인포인트 설정 -> 포인트컷 으로 설정 : 포인트컷은 조인포인트의 집합.
+	@Pointcut(value="execution(* com.myhome.web..*Controller.*(..))")
+	private void loggingControllerCut() {}
+	
+	@Pointcut(value="execution(* com.myhome.web.*.service.*Service.*(..))")
+	private void loggingServiceCut() {}
+	
+	@Pointcut(value="execution(* com.myhome.web.*.model.*DAO.*(..))")
+	private void loggingDaoCut() {}
+	
+	@Pointcut(value="loggingControllerCut() || loggingServiceCut() || loggingDaoCut()")
+	private void loggingMvcCut() {}
 	
 	// 어드바이스 작성 -> 메서드 생성
-	@Before(value="loggingCut()")
+	@Before(value="loggingMvcCut()")
 	public void beforeLogging(JoinPoint joinPoint) throws Exception {
-		logger.info("loggingAOP Test");
+		this._logging(joinPoint, "before");
+	}
+	
+	@After(value="loggingMvcCut()")
+	public void afterLogging(JoinPoint joinPoint) throws Exception {
+		this._logging(joinPoint, "after");
+	}
+	
+	private void _logging(JoinPoint joinPoint, String adviceType) throws Exception {
+		Date date = new Date();
+		
+		String fullyClassName = joinPoint.getSignature().getDeclaringTypeName();
+		String methodName = joinPoint.getSignature().getName();
+		
+		logger.info("[{}] - <<{}>>  - {}.{} ", dateFormat.format(date), adviceType.toUpperCase(), fullyClassName, methodName );
+
+		if(logger.isDebugEnabled()) {
+			for(Object arg: joinPoint.getArgs()) {
+				logger.debug("[{}] - Argument Type : {}", dateFormat.format(date), arg.getClass().getSimpleName());
+				logger.debug("[{}] - Argument Value : {}", dateFormat.format(date), arg);
+			}
+		}
 	}
 }
